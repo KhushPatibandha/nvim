decd06f5bb16204e00d2e7f54f53f5b115a80d6a
KhushPatibandha <khush.patibandha@gmail.com>
2024-04-09 23:34:03 +0530

    jdtls added and works

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,6 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
+                -- ensure_installed = { "lua_ls", "tsserver", "jdtls" }
                 ensure_installed = { "lua_ls", "tsserver", "jdtls" }
             })
         end
@@ -19,7 +20,7 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
-            lspconfig.jdtls.setup({})
+            -- lspconfig.jdtls.setup({})
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
