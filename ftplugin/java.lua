28e039e5ab6ed90224033a5648237900bced6817
KhushPatibandha <khush.patibandha@gmail.com>
2024-09-04 01:37:06 +0530

    java lsp fix

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,5 +2,38 @@ local config = {
     cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
     -- cmd = {'/path/to/jdt-language-server/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+    settings = {
+        java = {
+            signatureHelp = {
+                enabled = true,
+            },
+            sources = {
+                organizeImports = {
+                    starThreshold = 9999,
+                    staticStarThreshold = 9999,
+                },
+            },
+            codeGeneration = {
+                toString = {
+                    template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+                },
+                generateComments = true,
+                insertPackage = true,
+            },
+        },
+    },
+    capabilities = {
+        workspace = {
+            configuration = true,
+        },
+        textDocument = {
+            completion = {
+                completionItem = {
+                    snippentSupport = true,
+                },
+            },
+        },
+    },
 }
+config.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 require("jdtls").start_or_attach(config)
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,7 +24,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "jdtls", "gopls", "tsserver", "pyright" },
+                ensure_installed = { "lua_ls", "gopls", "tsserver", "pyright" },
             })
         end,
     },
@@ -35,7 +35,6 @@ return {
 
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.jdtls.setup({ capabilities = capabilities })
             lspconfig.gopls.setup({ capabilities = capabilities })
             lspconfig.tsserver.setup({ capabilities = capabilities })
             lspconfig.pyright.setup({ capabilities = capabilities })
