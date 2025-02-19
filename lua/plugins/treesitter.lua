60df48adb9e997e496d25a15340b80f2b380c766
KhushPatibandha <khush.patibandha@gmail.com>
2025-02-19 08:17:18 +0530

    --

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,25 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "java" },
-            auto_install = true,
-            sync_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "java" },
+                auto_install = true,
+                sync_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context")
+            vim.keymap.set("n", "[c", function()
+                require("treesitter-context").go_to_context(vim.v.count1)
+            end, { silent = true, desc = "Jump to upward context" })
+        end,
+    },
 }
