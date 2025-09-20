70065b6675625bcf7b96ca20b858fcb4781db26f
KhushPatibandha <khush.patibandha@gmail.com>
2025-09-21 00:50:33 +0530

    -

--- colorscheme.lua
+++ colorscheme.lua
@@ -1,1 +1,1 @@
-vim.cmd('colorscheme base16-gruber')
+vim.cmd('colorscheme base16-kanagawa')
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,24 +20,6 @@ return {
             vim.keymap.set("n", "<Leader><Leader>", builtin.buffers, { desc = "List open buffers" })
             vim.keymap.set(
                 "n",
-                "<Leader>tt",
-                ":Telescope treesitter<CR>",
-                { desc = "Search for symbols in current file with treesitter" }
-            )
-            vim.keymap.set(
-                "n",
-                "<Leader>ds",
-                builtin.lsp_document_symbols,
-                { desc = "Search for symbols in current file" }
-            )
-            vim.keymap.set(
-                "n",
-                "<Leader>ws",
-                builtin.lsp_dynamic_workspace_symbols,
-                { desc = "Search for symbols in whole project" }
-            )
-            vim.keymap.set(
-                "n",
                 "<Leader>ft",
                 ":TodoTelescope keywords=TODO,HACK,PERF,NOTE,FIX,WARNING<CR>",
                 { desc = "Search for marked comments. eg: TODO, FIX" }
@@ -52,18 +34,6 @@ return {
                 builtin.lsp_references,
                 { desc = "Get all the references in the project" }
             )
-
-            vim.keymap.set("n", "<Leader>f/", function()
-                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                    winblend = 0,
-                    previewer = true,
-                    layout_strategy = "horizontal",
-                    layout_config = {
-                        height = 0.85,
-                        width = 0.85,
-                    },
-                }))
-            end, { desc = "Fuzzily find in current buffers" })
 
             vim.keymap.set("n", "<Leader>kt", function()
                 builtin.colorscheme({
