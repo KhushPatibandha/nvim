58e89aa63fafb87754266377be95cecf8ebd43a7
Khush Patibandha <khush.patibandha@gmail.com>
2026-04-19 17:43:18 +0530

    --

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -53,10 +53,11 @@ return {
         })
 
         vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "java", "go" },
+            pattern = { "java", "go", "lua", "c", "cpp", "h", "hpp" },
             callback = function()
                 vim.opt_local.tabstop = 4
                 vim.opt_local.shiftwidth = 4
+                vim.opt_local.softtabstop = 4
                 vim.opt_local.expandtab = true
             end,
         })
